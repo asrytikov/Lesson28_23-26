072a7a113e1420b77abd97793b7fc32ae60aabd9
asrytikov <asrytikov@gmail.com>
2023-11-29 21:12:42 +0400

    Lesson28

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        People p = context.getBean(People.class);
+        System.out.println(p.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
+
+
+
     }
 }
--- src/main/java/org/example/ProjectConfig.java
+++ src/main/java/org/example/ProjectConfig.java
@@ -2,14 +2,41 @@ package org.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
 
-    @Bean
+    @Bean(name = "alex")
     People people(){
         var p = new People();
         p.setName("Alex");
         return p;
+    }
+
+    @Primary
+    @Bean(value = "masha")
+    People people2(){
+        var p = new People();
+        p.setName("Masha");
+        return p;
+    }
+
+    @Bean("andrey")
+    People people3(){
+        var p = new People();
+        p.setName("Andrey");
+        return p;
+    }
+
+
+    @Bean
+    String hello(){
+        return "Hello";
+    }
+
+    @Bean
+    Integer ten(){
+        return 10;
     }
 }
